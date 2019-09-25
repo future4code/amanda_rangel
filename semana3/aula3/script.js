ecec26842b704390c5239c73f3053b69acc8a19c
amandarangel <50452552+amandarangel@users.noreply.github.com>
2019-09-25 16:24:28 -0300

    exercicio-blog passo 4 com um bug

--- semana3/aula3/script.js
+++ semana3/aula3/script.js
@@ -1,13 +1,13 @@
 class Post {
-  constructor(titulo, autor, conteudo){
+  constructor(titulo, autor, conteudo) {
     this.titulo = titulo;
     this.autor = autor;
     this.conteudo = conteudo;
   }
-    criarNovoPost(){
-      const novoPost = this.titulo + ' ' + this.autor + ' ' + this.conteudo;
-        return novoPost;
-    }
+  criarNovoPost() {
+    const novoPost = this.titulo + ' ' + this.autor + ' ' + this.conteudo;
+    return novoPost;
+  }
 }
 
 const listaDePost = [];
@@ -16,16 +16,24 @@ function aoClicarEmPostar() {
   let tituloInserido = document.getElementById('titulo').value;
   let autorInserido = document.getElementById('autor').value;
   let conteudoInserido = document.getElementById('conteudo').value;
-    
-  const post1 = new Post(tituloInserido, autorInserido, conteudoInserido);
-  post1.criarNovoPost();
-    console.log(post1);
 
-  listaDePost.push(post1);
+  const post = new Post(tituloInserido, autorInserido, conteudoInserido);
+  post.criarNovoPost();
+  // console.log(post);
+
+  listaDePost.push(post);
   console.log(listaDePost);
 
   const limparTitulo = document.getElementById("titulo").value = "";
   const limparAutor = document.getElementById("autor").value = "";
   const limparConteudo = document.getElementById("conteudo").value = "";
+
+  const postagem = document.getElementById("area-post");
+
+  for (let post of listaDePost) {
+    postagem.innerHTML += '<div>' + post.criarNovoPost() + '</div>';
+  }
+  
 }
+
 
