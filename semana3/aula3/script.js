1bb30d30161874de57ebbb8a80604b72f948817d
amandarangel <50452552+amandarangel@users.noreply.github.com>
2019-09-25 15:43:55 -0300

    exercicio-blog passo 4

--- semana3/aula3/script.js
+++ semana3/aula3/script.js
@@ -10,14 +10,22 @@ class Post {
     }
 }
 
-
+const listaDePost = [];
 
 function aoClicarEmPostar() {
   let tituloInserido = document.getElementById('titulo').value;
   let autorInserido = document.getElementById('autor').value;
   let conteudoInserido = document.getElementById('conteudo').value;
-
+    
   const post1 = new Post(tituloInserido, autorInserido, conteudoInserido);
   post1.criarNovoPost();
     console.log(post1);
+
+  listaDePost.push(post1);
+  console.log(listaDePost);
+
+  const limparTitulo = document.getElementById("titulo").value = "";
+  const limparAutor = document.getElementById("autor").value = "";
+  const limparConteudo = document.getElementById("conteudo").value = "";
 }
+
