ddcc5f6ee6c9b1d6745eb805e1fff63b28eb2406
amandarangel <50452552+amandarangel@users.noreply.github.com>
2019-09-27 08:09:45 -0300

    controlador de despesas feito até o ponto 5

--- semana3/aula4/script.js
+++ semana3/aula4/script.js
@@ -1,9 +1,10 @@
 
 function limpaCampos(){
   document.getElementById("valor").value = "";
-  document.getElementById("tipo-despesa-cadastro").value = "";
   document.getElementById("descrição").value = "";
 }
+
+// cria classe para a despesa
 
 class Despesa {
   constructor(valor, tipo, descricao) {
@@ -11,30 +12,67 @@ class Despesa {
     this.tipo = tipo;
     this.descricao = descricao;
   }
+  compilaDadosDespesa() {
+    const novaDespesa = "<li>" + "<p>" + "Valor: " + this.valor + "</p>" + "<p>" + "Tipo: " + this.tipo + "</p>" + "<p>" + "Descrição: " + this.descricao + "</p>" + "<li>" + "<hr>";
+    return novaDespesa;
+  }
 }
 
-const listaDeDespesas = [];
+// cria array para armazenar as despesas inseridas
 
-const valor = document.getElementById("valor").value;
-const tipo = document.getElementById("tipo-despesa-cadastro").value;
-const descricao = document.getElementById("descricao").value;
+let listaDeDespesas = [];
 
 
 function criaDespesa() {
+// captura dados dos inputs
+  const valor = document.getElementById("valor").value;
+  const tipo = document.getElementById("tipo-despesa-cadastro").value;
+  const descricao = document.getElementById("descricao").value;
 
-  
+// impede que sejam criadas instancias novas sem preenchimento dos campos 
   if(valor === '' || descricao === ''){
    
     alert("Preencha todos os campos antes de prosseguir");
     return;
   }
 
-  let novaDespesa = new Despesa(valor, tipo, descricao);
+// limpa os campos de input após clicar no botão cadastrar
+  const limpaValor = document.getElementById("valor").value = "";
+  const limpaDescricao = document.getElementById("descricao").value = "";
 
-   listaDeDespesas.push(novaDespesa);
-     console.log(listaDeDespesas);
+ // cria nova instancia de despesa
+  let novaDespesa = new Despesa(valor, tipo, descricao);
+// armazena nova instancia na array
+  listaDeDespesas.push(novaDespesa);
+    console.log(listaDeDespesas);
+    imprimeTodasDespesas();
+}
+  
+// imprime as despesas no html
+function imprimeTodasDespesas() {
+  const areaDasDespesas = document.getElementById("area-despesas");
+// limpa area para evitar info duplicadas
+  areaDasDespesas.innerHTML = "";
+// adiciona despesa na area de despesa
+  for (let despesa of listaDeDespesas){
+    areaDasDespesas.innerHTML += despesa.compilaDadosDespesa();
+  }
 }
 
-
-
-
+function aoClicarEmFiltrar(){
+  const filtraTipo = listaDeDespesas.filter((despesa, index, array) => {
+    if (despesa.tipo === "casa" || despesa.tipo === "transporte" || despesa.tipo === "alimentação"){
+        return true;
+      } 
+      return false;
+  })
+  console.log(filtraTipo);
+}
+//   const filtraTipoTransp = listaDeDespesas.filter((despesa, index, array) => {
+//     if (despesa.tipo === "transporte"){
+//         return true;
+//       } 
+//       return false;
+//   })
+//   console.log(filtraTipoTransp);
+// }
