125b55f384c79d4138e750a8e7904fce367a6850
amandarangel <50452552+amandarangel@users.noreply.github.com>
2019-10-08 07:33:08 -0300

    Task List (muito incompleta)

---
+++ semana5/aula1/task-list/src/components/DoneTasksCard/DoneTasksCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const CampoMsgsCardContainer = styled.div`
+
+`
+
+class CampoMsgsCard extends React.Component {
+  constructor(props){
+    super(props);
+  }
+    render(){
+      return (
+        <CampoMsgsCardContainer>
+           
+        </CampoMsgsCardContainer>
+      );
+    }
+}
+
+  CampoMsgsCard.propTypes = {
+   
+  }
+  
+export default CampoMsgsCard;
