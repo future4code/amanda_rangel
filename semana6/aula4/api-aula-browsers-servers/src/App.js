f3fdef998d617e52270e1a8b098305155c234015
amandarangel <50452552+amandarangel@users.noreply.github.com>
2019-10-18 11:17:28 -0300

    Projeto quase finalizado

---
+++ semana6/aula4/api-aula-browsers-servers/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PokemonSelection from './components/PokemonSelection'
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonSelection />
+    </div>
+  );
+}
+
+export default App;
