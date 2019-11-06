f04a1f7ad95e2da5e88600e91679b862c17efe05
amandarangel <50452552+amandarangel@users.noreply.github.com>
2019-11-05 21:45:00 -0300

    começando a trabalhar no layout da home

--- semana9/aula1/astroTrips/src/containers/HomePage/index.js
+++ semana9/aula1/astroTrips/src/containers/HomePage/index.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { push } from "connected-react-router";
-import {routes} from '../Router'
+import { routes } from '../Router'
 import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import logo2 from "../../images/logo2.png"
+import logo from "../../images/logo.png"
+import astronaut from "../../images/astronaut.jpg"
 import styled from "styled-components";
 
-const HomeWrapper = styled.form`
-  width: 100%;
-  height: 100vh;
-  gap: 10px;
-  place-content: center;
-  justify-items: center;
-  display: grid;
+const HomeWrapper = styled.div`
+ width: 100vw;
 `;
+const AppBarStyled = styled(AppBar)`
+
+`
+
+const ToolbarStyled = styled(Toolbar)`
+  background-color: #193A4D;
+`
+const Name = styled.img`
+  height: 30px;
+`
+const Logo = styled.img`
+  height: 30px;
+`
+const HomeImg = styled.img`
+  width: 100vw;
+  height: 70vh;
+`
+const ButtonStyled = styled(Button)`
+  background-color: #193A4D;
+  color: white;
+`
+
 
 class HomePage extends Component {
   constructor(props) {
@@ -24,8 +46,21 @@ class HomePage extends Component {
     
     return (
       <HomeWrapper>
-        <Button onClick={this.props.goToApplicationForm}>Inscreva-se aqui!</Button>
-        <Button onClick={this.props.goToLogin}>Login</Button>
+        <AppBarStyled position="static">
+        <ToolbarStyled>
+          <Name src={logo2} />
+          {/* <Logo src={logo} /> */}
+        </ToolbarStyled>
+        </AppBarStyled>
+        <HomeImg src={astronaut} />
+        <ButtonStyled 
+          variant="contained" 
+          onClick={this.props.goToApplicationForm}>Inscreva-se aqui!
+        </ButtonStyled>
+        <ButtonStyled 
+          variant="contained" 
+          onClick={this.props.goToLogin}>Login
+        </ButtonStyled>
       </HomeWrapper>
     );
   }
