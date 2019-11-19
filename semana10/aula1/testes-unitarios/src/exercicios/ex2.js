075c19500e1c45dea63c3b12ea2cd9890c9c249f
amandarangel <50452552+amandarangel@users.noreply.github.com>
2019-11-18 22:11:25 -0300

    exercício 2

--- semana10/aula1/testes-unitarios/src/exercicios/ex2.js
+++ semana10/aula1/testes-unitarios/src/exercicios/ex2.js
@@ -1,1 +1,16 @@
-export function checaPalindromo(frase) {}
+import { reverse } from "dns";
+
+export function checkPalindrome (string) {
+   const incomeString = string.toLowerCase().replace(/[-\s,]/g, "").replace(/é/gi, "e").replace(/á/gi, "a").replace(/!/gi, "").replace(/,/gi, "")
+   const outcomeString = incomeString
+      .split('')
+      .reverse()
+      .join('');
+    return incomeString === outcomeString;
+}   
+
+  
+
+
+
+
