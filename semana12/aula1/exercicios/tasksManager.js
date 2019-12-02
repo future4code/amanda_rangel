71b63a06c20d90a53963b4024d92ee707e65394d
amandarangel <50452552+amandarangel@users.noreply.github.com>
2019-12-02 16:00:47 -0300

    exercício 2 completo

--- semana12/aula1/exercicios/tasksManager.js
+++ semana12/aula1/exercicios/tasksManager.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const fileName = process.argv[2];
-const task1 = 'fazer mala /nestudar'
-// const task3 = `\n${process.argv[5]}`;
+const task1 =  'fazer mala';
+const task2 = 'estudar';
+const task3 = process.argv[3];
 
 try {
-  fs.writeFileSync(fileName, task1);
+  fs.writeFileSync(fileName, task1 + "\n" + task2);
   console.log("Arquivo criado com sucesso!");
-  // fs.appendFileSync(fileName, task2, 'utf8');
-  // console.log("Tarefa adicionada com sucesso!");
+  fs.appendFileSync(fileName, "\n" + task3, 'utf8');
+  console.log("Tarefa adicionada com sucesso!");
 } catch (e) {
   console.log(e)
 }
