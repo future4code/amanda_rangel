c776504635dfa1a191f98d2f1e854219cefd68b3
amandarangel <50452552+amandarangel@users.noreply.github.com>
2019-12-10 16:46:10 -0300

    implementado até o ponto 5

---
+++ semana14/aula2/restaurantManager/src/employee.ts
@@ -0,0 +1,9 @@
+export abstract class Employee {
+  protected name: string;
+  protected salary: number;
+  constructor(name: string, salary: number) {
+    this.name = name;
+    this.salary = salary
+  }
+  abstract sayJob(): string
+}
